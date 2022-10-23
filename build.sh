209e9888b5c709975a8295fef8155ccb3793aa58
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-23 21:54:39 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
+
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b queso -g default,-mips,-darwin,-notdefault
-git clone https://github.com/saitama-sense/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-corvus-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_whyred-userdebug
+lunch corvus_whyred-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=Saitama
-export KBUILD_BUILD_HOST=Cirrus
-export BUILD_USERNAME=Saitama
-export BUILD_HOSTNAME=Cirrus
+export KBUILD_BUILD_USER=zacky
+export KBUILD_BUILD_HOST=jenkins
+export BUILD_USERNAME=zacky
+export BUILD_HOSTNAME=jenkins
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka bacon-j8
-# build 1
+mka corvus -j8
+# build CorvusOS 14
