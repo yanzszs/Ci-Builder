86eb8817265f2d864d6f8b9f4c93f6c1e72c3b19
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-23 08:07:49 +0700

    Update [skip ci]

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b tiramisu -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-evo-x-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-AOSP/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-corvus-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch evolution_whyred-userdebug
+lunch corvus_whyred-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
-export KBUILD_BUILD_HOST=cirrus
+export KBUILD_BUILD_HOST=jenkins
 export BUILD_USERNAME=zacky
-export BUILD_HOSTNAME=cirrus
+export BUILD_HOSTNAME=jenkins
 # export USE_MICROG=true
-export TARGET_USES_MINI_GAPPS=true
-mka evolution -j8
-# build 8
+# export TARGET_USES_MINI_GAPPS=true
+mka corvus -j8
+# build CorvusOS
