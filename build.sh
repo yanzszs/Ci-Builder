450491d30ea2eefeb9b0a483c6d3675ff3dcc58a
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-25 04:40:52 +0700

    Ginkgo [skip ci]

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/BootleggersROM/manifest.git -b queso -g default,-mips,-darwin,-notdefault
-git clone https://github.com/maung-bodaz/local_manifest --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch bootleg_whyred-userdebug
+lunch banana_ginkgo-userdebug
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=maung-bodaz
+export KBUILD_BUILD_USER=yanzszs
 export KBUILD_BUILD_HOST=jenkins
-export BUILD_USERNAME=maung-bodaz
+export BUILD_USERNAME=yanzszs
 export BUILD_HOSTNAME=jenkins
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 # export USE_MICROG=true
 # export TARGET_USES_MINI_GAPPS=true
-mka bacon -j8
-# build bootleggers 
+m banana -j8
+# build bananadroid
