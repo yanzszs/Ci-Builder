c43813a40889caeca3bc2b1f75beb21e2f211e02
yanzszs <115848467+yanzszs@users.noreply.github.com>
2022-10-27 04:21:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b whyred-13 .repo/local_manifests
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b rice-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
