935bc640b388757de766c8ca1d33a993f5cf055a
yanz <115848467+yanzszs@users.noreply.github.com>
2022-11-04 10:45:34 +0700

    build: ginkgo: BananaDroid 13 Tanduk

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/RiceDroid/android -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/yanzszs/local_manifest --depth 1 -b rice-13-ginkgo .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/yanzszs/local_manifest --depth 1 -b banana-13-ginkgo .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_ginkgo-userdebug
+lunch banana_ginkgo-userdebug
 export TZ=Asia/Jakarta
 export KBUILD_BUILD_USER=zacky
 export KBUILD_BUILD_HOST=android-build
